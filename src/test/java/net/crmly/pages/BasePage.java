65e950bcc0b3393ce90770dbedf77213ac2adc8e
serhanalp <serhanalpaydin@gmail.com>
2023-02-28 18:58:53 -0600

    Base Page changes done

--- src/test/java/net/crmly/pages/BasePage.java
+++ src/test/java/net/crmly/pages/BasePage.java
@@ -1,20 +1,47 @@
 package net.crmly.pages;
 
 import net.crmly.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public abstract class BasePage {
 
-    public BasePage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    // This page contains common WebElements and methods for all pages.
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(xpath = "//a[@class='menu-item-link']")
+    public List<WebElement> menuOptions;    // left panel options as a List
+
+    @FindBy(className = "header-search-input")
+    public WebElement searchBar;
+
+    @FindBy(css = "#user-block")
+    public WebElement userProfileDropdown;   // top right of the screen where user email is shown
+
+    @FindBy(xpath = "//span[@class='menu-popup-item-text'][normalize-space()]")
+    public List<WebElement> userProfileDropdownOptions;    // user profile dropdown options as a List
 
 
 
+    /** This method lets us click on a desired Base Page WebElement contained in a List
+     *  (such as Left panel options or user settings dropdown options)
+     * @param options
+     * @param optionName
+     */
+    public void clickOnOption(List<WebElement> options, String optionName){
 
-
-
+        for (WebElement each : options) {
+            if (each.getText().equals(optionName.trim().toLowerCase())){
+                each.click();
+            }
+        }
+    }
 
 
 
