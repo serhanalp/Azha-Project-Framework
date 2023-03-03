8bf47d61f8819c1d4d21468d052c0d2a1e4caf4b
miladjaberi1369 <114787297+miladjaberi1369@users.noreply.github.com>
2023-03-02 19:39:25 -0800

    CompanyPage Revision 1

---
+++ src/test/java/net/crmly/step_definitions/US86_CompanyPage_StepDef.java
@@ -0,0 +1,56 @@
+package net.crmly.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.crmly.pages.CompanyPage;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class US86_CompanyPage_StepDef
+{
+
+    CompanyPage companyPage = new CompanyPage();
+
+
+    @When("user goes to the company page")
+    public void user_goes_to_the_company_page() {
+
+
+        companyPage.companyPageClick.click();
+
+
+
+
+
+
+    }
+    @Then("should see the seven module in the page")
+    public void should_see_the_seven_module_in_the_page(List<String> companyPageMenu) {
+
+
+
+        List<String> companyPageMenuTxt = new ArrayList<>();
+        for (WebElement each : companyPage.companyPageMenue)
+        {
+            companyPageMenuTxt.add(each.getText());
+        }
+
+        Collections.sort(companyPageMenuTxt);
+        Collections.sort(companyPageMenu);
+
+
+        Assert.assertEquals(companyPageMenu,companyPageMenuTxt);
+
+
+
+
+
+
+    }
+}
+
+
